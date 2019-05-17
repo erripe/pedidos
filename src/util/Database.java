502e3446f25e4fa73d6e6af1ab18fc76269c0e56
Emmanoel Loss <emmanoell@gmail.com>
2019-05-16 21:57:23 -0300

    Add dos packages util e app. Add das classes para BD.

---
+++ src/util/Database.java
@@ -0,0 +1,42 @@
+package util;
+
+public class Database {
+	String driverClass;
+	String caminho;
+	String user;
+	String senha;
+	
+	public Database(String driverClass, String caminho, String user, String senha) {
+		super();
+		this.driverClass = driverClass;
+		this.caminho = caminho;
+		this.user = user;
+		this.senha = senha;
+	}
+
+	public Database() {
+		super();
+		//jdbc:mysql://localhost/
+		this.driverClass = "com.mysql.cj.jdbc.Driver";
+		this.caminho = "jdbc:mysql://localhost/sistemaVendas?useTimezone=true&serverTimezone=UTC";
+		this.user = "root";
+		this.senha = "";
+	}
+
+	public String getDriverClass() {
+		return driverClass;
+	}
+
+	public String getCaminho() {
+		return caminho;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+	
+}
